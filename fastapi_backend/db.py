82f580c0bcfb514a2578b01b6576c5a7641d2d70
Jesus Jahir Delgado Nuñez <al215650@alumnos.uacj.mx>
2025-05-21 10:43:08 -0600

    Update and rename db.py to database.py

--- fastapi_backend/db.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-MONGO_URI = os.getenv("MONGO_URI")  # o puedes pegar tu URI directamente aquí
-client = MongoClient(MONGO_URI)
-db = client["citasSalon"]
