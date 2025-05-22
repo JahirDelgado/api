f34968d17c89d831bbe07b256662fd86de9f3a46
Jesus Jahir Delgado Nuñez <al215650@alumnos.uacj.mx>
2025-05-22 07:52:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import auth, citas, recordatorios, usuarios
+from routes import auth, citas, recordatorios, usuarios, insumos
 
 app = FastAPI(
     title="BeautyTech API",
@@ -20,6 +20,7 @@ app.include_router(auth.router)
 app.include_router(citas.router, prefix="/citas", tags=["Citas"])
 app.include_router(recordatorios.router)
 app.include_router(usuarios.router, prefix="/usuarios", tags=["Usuarios"])
+app.include_router(insumos.router)
 
 @app.get("/")
 def read_root():
